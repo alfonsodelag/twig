2c9c26d137c2bba01b53e3c368919f5f3d0906a6
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-02-13 15:22:56 +0100

    iterating the news

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use Twig\Loader\FilesystemLoader;
 $loader = new Twig\Loader\FilesystemLoader('templates');
 $twig = new Twig\Environment($loader);
 
+$date = new DateTime('2020-01-01');
 // $news = array
 // (
 //     newsTitle = array("Title1", "Title2", "Title3")
@@ -15,8 +16,24 @@ $twig = new Twig\Environment($loader);
 //     newsDescription = array("Description1", "Description2", "Description3")
 // )
 
-$template = $twig->load('home.html.twig');
+// $template = $twig->load('news.html.twig');
 
-echo $template->render();
+
+
+// echo $template->render('news.html.twig', array(array(news1 => 'Sport', 'date' => $date->format('Y-m-d'), 'description' => 'Lorem Ipsum')
+// array(news2 => 'Business', 'date' => $date->format('Y-m-d'), 'description' => 'Lorem Ipsum')
+// array(news3 => 'Sport', 'Travels' => $date->format('Y-m-d'), 'description' => 'Lorem Ipsum')));
+
+$news = array (
+    'sections' => array(
+        array('title' => 'Sport', 'date' => $date->format('Y-m-d'), 'description' => 'Lorem Ipsum'),
+        array('title' => 'Business', 'date' => $date->format('Y-m-d'), 'description' => 'Lorem Ipsum'),
+        array('title' => 'Travels', 'date' => $date->format('Y-m-d'), 'description' => 'Lorem Ipsum')
+        )
+    );
+
+    // echo $twig->render('news.html.twig', $news);
+    echo $twig->render('empty.html.twig', $news);
+
 
 ?>
